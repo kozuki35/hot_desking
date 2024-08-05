2bd9c0202f78a33a48fc40d774314cd7a5b09b99
Yueqing-Chen-1158988 <yueqing.chen@lincolnuni.ac.nz>
2024-08-05 21:56:47 +1200

    Add router path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <div>{routes}</div>;
 };
 
 export default App;
