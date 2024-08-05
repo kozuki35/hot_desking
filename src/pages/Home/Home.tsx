d3440f4a444dca911fbdd3b9886e3e1bdc939804
Yueqing-Chen-1158988 <yueqing.chen@lincolnuni.ac.nz>
2024-08-05 21:48:35 +1200

    project init & quick frontend scaffold

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Login from '../Login/Login';
+import SignUp from '../SignUp/SignUp';
+
+const Home = () => {
+  return (
+    <div>
+      <Login />
+      <SignUp />
+    </div>
+  );
+};
+
+export default Home;
